ab5aca794e6245dbf84d498e0b88281a962c82f7
Hai Le <haile.vnm@gmail.com>
2023-11-09 15:37:58 +0700

    feat(examples): using can-it/core as local

--- apps/examples/src/main.ts
+++ apps/examples/src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hello World');
+import { CanIt } from '@can-it/core-local';
+
+const canIt = new CanIt({
+  allow: [
+    ['view', 'documents'],
+    ['edit', 'documents']
+  ],
+  deny: [
+    ['edit', 'documents']
+  ]
+});
+
+console.log(canIt.allowTo('edit', 'documents'));
+console.log(canIt.allowTo('view', 'documents'));
