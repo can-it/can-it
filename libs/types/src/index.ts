914902072b46ee7b284990e8e1145caceeb4c7f5
Hai Le <haile.vnm@gmail.com>
2023-11-28 11:09:42 +0700

    feat(types): add resource store interface

--- libs/types/src/index.ts
+++ libs/types/src/index.ts
@@ -2,4 +2,5 @@ export * from './lib/comparator';
 export * from './lib/generator';
 
 export * from './lib/policy-state';
+export * from './lib/resource-state';
 export * from './lib/request';
---
+++ libs/types/src/lib/resource-state.ts
@@ -0,0 +1,4 @@
+export interface ResourceState<T = unknown> {
+  ri: string;
+  resource?: T;
+}
