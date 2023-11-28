e785b444e3870342b125a27e05fe8bad0aaab736
Hai Le <haile.vnm@gmail.com>
2023-11-28 10:18:14 +0700

    feat(nest): implement the LoadPolicyMiddleware

--- libs/nest/src/lib/middlewares/load-policy.middleware.ts
+++ libs/nest/src/lib/middlewares/load-policy.middleware.ts
@@ -1,12 +1,17 @@
-import { Injectable, NestMiddleware, Scope } from '@nestjs/common';
+import { Inject, Injectable, NestMiddleware, Scope } from '@nestjs/common';
+import { PolicyStore } from '../services/policy-store.service';
+import { POLICY_RESOLVER } from '../token';
+import { PolicyState } from '@can-it/types';
 
 @Injectable({ scope: Scope.REQUEST })
 export class LoadPolicyMiddleware implements NestMiddleware {
-  constructor() {
+  constructor(
+    private storeService: PolicyStore,
+    @Inject(POLICY_RESOLVER) private policyResolver: (req: unknown) => PolicyState
+  ) {}
 
-  }
-
-  use(req: any, res: any, next: () => void) {
+  async use(req: unknown, _res: unknown, next: () => void) {
+    this.storeService.set(await this.policyResolver(req))
     next();
   }
 }
