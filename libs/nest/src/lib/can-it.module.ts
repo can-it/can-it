0c1f76d19fe9540f3a2ddfd646885eeceff22b62
Hai Le <haile.vnm@gmail.com>
2023-11-28 17:38:28 +0700

    feat(nest): config can-it-module

--- libs/nest/src/lib/can-it.module.ts
+++ libs/nest/src/lib/can-it.module.ts
@@ -1,8 +1,36 @@
-import { Module } from '@nestjs/common';
+import { Comparator, PolicyState } from '@can-it/types';
+import { DynamicModule, Module } from '@nestjs/common';
+import { COMPARATORS, POLICY_RESOLVER, RI_RESOLVER } from './constants';
+import { RiResolver } from './models/ri-resolver';
+import { RequestConsumer } from './models/policy-resolver';
+
+export interface CanItConfiguration {
+  comparators?: { action?: Comparator, ri?: Comparator },
+  resolvers?: { ri?: RiResolver, policy?: RequestConsumer<PolicyState> }
+}
 
 @Module({
   controllers: [],
   providers: [],
   exports: [],
 })
-export class CanItModule {}
+export class CanItModule {
+  static register(config: CanItConfiguration): DynamicModule {
+    const { comparators, resolvers } = config;
+
+    return {
+      module: CanItModule,
+      providers: [
+        { provide: COMPARATORS, useValue: comparators },
+        { provide: POLICY_RESOLVER, useValue: resolvers?.policy },
+        { provide: RI_RESOLVER, useValue: resolvers?.ri }
+      ]
+    }
+  }
+
+  static forChild(): DynamicModule {
+    return {
+      module: CanItModule
+    }
+  }
+}
