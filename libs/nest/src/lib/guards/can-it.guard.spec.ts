bad4da83a52c6230ebe1193a92f58785489a4e41
Hai Le <haile.vnm@gmail.com>
2023-12-05 14:57:24 +0700

    test(nest): quick fix the test

--- libs/nest/src/lib/guards/can-it.guard.spec.ts
+++ libs/nest/src/lib/guards/can-it.guard.spec.ts
@@ -1,7 +1,16 @@
+import { Reflector } from '@nestjs/core';
 import { CanItGuard } from './can-it.guard';
+import { Test } from '@nestjs/testing';
 
 describe('CanItGuard', () => {
-  it('should be defined', () => {
-    expect(new CanItGuard()).toBeDefined();
+  it('should be defined', async () => {
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        Reflector,
+        CanItGuard
+      ]
+    }).compile();
+
+    expect(moduleRef.get(CanItGuard)).toBeDefined();
   });
 });
